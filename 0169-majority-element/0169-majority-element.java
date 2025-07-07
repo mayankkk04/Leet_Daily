5e69450b9aeaad826bf5c1a312eb9e18b512e2cb
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-07-08 00:39:56 +0530

    Time: 2 ms (73.86%), Space: 53.6 MB (8.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,11 +2,25 @@ class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < n ; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i]) > n/2) return nums[i];
+        int cnt = 1;
+        int res = 0;
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] == nums[res]){
+                cnt++;
+            }
+            else cnt--;
+            if(cnt == 0){
+                res = i;
+                cnt = 1;
+            }
         }
-        return -1;
+        //till here we got our probable majority element .
+        //as rest of them will get cancelled out
+        for(int i = 0 ; i < n ; i++){
+            if(nums[res] == nums[i]) cnt++;
+        }
+        if(cnt < n/2) return -1;
+        return nums[res];
     }
+
 }
