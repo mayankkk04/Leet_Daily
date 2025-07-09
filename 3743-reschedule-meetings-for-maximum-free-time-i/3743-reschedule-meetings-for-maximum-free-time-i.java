fad407f85df931165c00e6e23f6c54d00120c899
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-07-09 19:16:19 +0530

    Time: 4 ms (40.56%), Space: 57.2 MB (93.33%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -4,16 +4,17 @@ class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
         int n = startTime.length;
         ArrayList<Integer> free = new ArrayList<>();
-        free.add(startTime[0]);
+        free.add(startTime[0]); //incase there is free space before start of 1st event
         for(int i = 1 ; i < n ; i++){
             free.add(startTime[i] - endTime[i-1]);
         }
-        free.add(eventTime - endTime[n-1]);
+        free.add(eventTime - endTime[n-1]); //incase there is free space after last event
         int i = 0;
         int j = 0;
         int maxSum = 0;
         int currsum = 0;
         int m = free.size();
+        //apply sliding window
         while(j < m){
             currsum += free.get(j);
             if(i < n && j-i+1 > k+1){
