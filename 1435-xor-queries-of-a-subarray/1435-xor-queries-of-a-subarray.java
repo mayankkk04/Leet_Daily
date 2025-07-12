f7d6995086551d5b980df19e1e87a46a918d8d40
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-07-12 23:35:55 +0530

    Time: 6 ms (17.49%), Space: 60.9 MB (41.4%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        ArrayList<Integer> ans = new ArrayList<>();
         int n = arr.length;
-        int m = queries.length;
-        for(int i = 0 ; i < m ; i++){
-                int xor = 0;
-                int left = queries[i][0];
-                int right = queries[i][1];
-                for(int k = left ;  k <= right ; k++){
-                    xor ^= arr[k];
-                }
-                ans.add(xor);
+        int[] cumXOR = new int[n];
+        cumXOR[0] = arr[0];
+        for(int i = 1 ; i < n ; i++){
+            cumXOR[i] = cumXOR[i-1]^arr[i];
         }
-        int[] res = new int[ans.size()];
-        for(int i = 0 ; i < ans.size() ; i++){
-            res[i] = ans.get(i);
+        List<Integer> res = new ArrayList<>();
+        for(int[] query : queries){
+            int l = query[0];
+            int r = query[1];
+            int xor = cumXOR[r]^(l == 0 ? 0 : cumXOR[l-1]);
+            res.add(xor);
         }
-        return res;
+        int[] solution = new int[res.size()];
+        for(int i = 0 ; i < res.size() ; i++){
+            solution[i] = res.get(i);
+        }
+        return solution;
     }
 }
