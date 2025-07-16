0c35245463abb5b7e995ff67f03f4b6878212381
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-07-17 00:29:10 +0530

    Time: 1 ms (100%), Space: 47.7 MB (67.78%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,28 +1,23 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
+        return function(nums,goal) - function(nums,goal-1);
+    }
+    private int function(int[] nums, int goal){
+        if (goal < 0) return 0;  
+        int l = 0;
         int n = nums.length;
-        int res = 0;
-        int cntzero = 0;
-        int winsum = 0;
-        int i = 0;
-        int j = 0;
-        while(j < n){
-            winsum += nums[j];
-            while(i < j && (nums[i] == 0 || winsum > goal) ){
-                if(nums[i] == 1){
-                    cntzero = 0;
-                }
-                else{
-                    cntzero++;
-                }
-                winsum -= nums[i];
-                i++;
+        int sum = 0;
+        int cnt = 0;
+        int r = 0;
+        while(r < n){
+            sum += nums[r];
+            while(sum > goal){
+                sum = sum - nums[l];
+                l++;
             }
-            if(winsum == goal){
-                res += 1+cntzero;
-            }
-            j++;
+            cnt = cnt + (r-l+1);
+            r = r+1;
         }
-        return res;
+        return cnt;
     }
 }
