f8e67da437455bfcf2cbb0103ff1f9a1fb572492
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-07-18 01:44:00 +0530

    Time: 10 ms (67.96%), Space: 44.6 MB (82.43%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -10,12 +10,9 @@ class Solution {
             char ch = s.charAt(r);
             hash[ch-'A']++;
             maxfreq = Math.max(maxfreq,hash[ch-'A']);
-            while((r-l+1) - maxfreq > k){
+            if((r-l+1) - maxfreq > k){
                 hash[s.charAt(l) - 'A']--;
                 maxfreq = 0;
-                for(int i = 0 ; i < 26 ; i++){
-                    maxfreq = Math.max(maxfreq,hash[i]);
-                }
                 l++;
             }
             if((r-l+1) - maxfreq <= k){
