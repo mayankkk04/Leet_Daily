6ef543d0760343947713882bb971fbf99036e37e
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-07-21 21:01:54 +0530

    Time: 0 ms (100%), Space: 44.8 MB (36.64%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even; // To connect later
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead; // Attach even list after odd list
+        return head;
+    }
+}
