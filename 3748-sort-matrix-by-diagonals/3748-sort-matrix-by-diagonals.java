da5303b476cae83a7b7c0bea4dd4d38f4dfdf26a
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2025-08-29 11:00:34 +0530

    Time: 4 ms (93.36%), Space: 45.6 MB (57.52%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
@@ -1,25 +1,47 @@
-import java.util.*;
 class Solution {
-    public int[][] sortMatrix(int[][] matrix) {
-        Map<Integer, PriorityQueue<Integer>> diagonalMap = new HashMap<>();
-        int rows = matrix.length, cols = matrix[0].length;
+    int n;
+    private void sortDiag(int row, int clm, int[][] grid, boolean ascend){
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = row;
+        int j = clm;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int key = i - j;
-                diagonalMap.putIfAbsent(key, key < 0 ? new PriorityQueue<>() 
-                                                     : new PriorityQueue<>(Collections.reverseOrder()));
-                diagonalMap.get(key).offer(matrix[i][j]);
-            }
+        while(i < n && j < n){
+            list.add(grid[i][j]);
+            i++;
+            j++;
+        }
+        
+        if(ascend){
+            Collections.sort(list);
+        }else{
+            Collections.sort(list, Collections.reverseOrder());
         }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int key = i - j;
-                matrix[i][j] = diagonalMap.get(key).poll();
-            }
+        i = row;
+        j = clm;
+        for (int k = 0; k < list.size(); k++) {
+            grid[i][j] = list.get(k);
+            i++;
+            j++;
+        }
+    }
+
+    public int[][] sortMatrix(int[][] grid) {
+        n = grid.length;
+        int row = 0;
+        int clm = 0;
+
+        //bottom-left
+        for(row = 0 ; row < n ; row++){
+            sortDiag(row,0,grid,false);
         }
 
-        return matrix;
+        //top-right
+        for(clm = 1 ; clm < n ; clm++){
+            sortDiag(0,clm,grid,true);
+        }
+
+        return grid;
+
     }
 }
